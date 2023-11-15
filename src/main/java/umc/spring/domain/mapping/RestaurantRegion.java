81cdc3592fb939c0951ad73d4a7ea232076420bb
GyuHyeokjjookki <jjookki33@gmail.com>
2023-11-15 15:31:38 +0900

    feat/식당별 위치 정보 저장을 위한 테이블 생성 코드 작성

---
+++ src/main/java/umc/spring/domain/mapping/RestaurantRegion.java
@@ -0,0 +1,28 @@
+package umc.spring.domain.mapping;
+
+import lombok.*;
+import umc.spring.domain.Region;
+import umc.spring.domain.Restaurant;
+import umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class RestaurantRegion extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+}
