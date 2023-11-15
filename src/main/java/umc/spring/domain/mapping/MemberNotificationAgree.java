46b2fd0dc9f903ab7f3f8fd30d2842dfc6914af3
gyu_hyeok <jjookki33@gmail.com>
2023-11-16 00:41:38 +0900

    feat/회원별 알람 수신 동의 테이블 소스 코드 수정

--- src/main/java/umc/spring/domain/mapping/MemberNotificationAgree.java
+++ src/main/java/umc/spring/domain/mapping/MemberNotificationAgree.java
@@ -3,6 +3,7 @@ package umc.spring.domain.mapping;
 import lombok.*;
 import umc.spring.domain.Member;
 import umc.spring.domain.NotificationType;
+import umc.spring.domain.common.BaseEntity;
 
 import javax.persistence.*;
 
@@ -11,7 +12,7 @@ import javax.persistence.*;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class MemberNotificationAgree {
+public class MemberNotificationAgree extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
