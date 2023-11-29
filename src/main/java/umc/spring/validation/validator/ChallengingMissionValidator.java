4e05b3be0f52e48d5a749408a982ae0d678e5787
gyu_hyeok <jjookki33@gmail.com>
2023-11-30 02:02:44 +0900

    :sparkles: Feat: 가게의 미션을 도전 중인 미션에 추가하기 API 구현
    validation 함수 구현

---
+++ src/main/java/umc/spring/validation/validator/ChallengingMissionValidator.java
@@ -0,0 +1,37 @@
+package umc.spring.validation.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.spring.apiPayload.code.status.ErrorStatus;
+import umc.spring.domain.enums.MissionStatus;
+import umc.spring.domain.mapping.MemberMission;
+import umc.spring.service.MemberMissionService.MemberMissionQueryService;
+import umc.spring.validation.annotation.ChallengingMissions;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Component
+@RequiredArgsConstructor
+public class ChallengingMissionValidator implements ConstraintValidator<ChallengingMissions, Long> {
+
+    private final MemberMissionQueryService memberMissionQueryService;
+
+    @Override
+    public void initialize(ChallengingMissions constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(Long value, ConstraintValidatorContext context) {
+//        MemberMission memberMission = memberMissionQueryService.findMemberMission(value);
+//
+//        if(memberMission.getStatus() == MissionStatus.CHALLENGING){
+//            context.disableDefaultConstraintViolation();
+//            context.buildConstraintViolationWithTemplate(ErrorStatus.INPROGRESS_MISSION.toString())
+//                    .addConstraintViolation();
+//            return false;
+//        }
+        return true;
+    }
+}
