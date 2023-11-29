2046c2b3f94ffba072456c8b75d21e7552cd87ea
gyu_hyeok <jjookki33@gmail.com>
2023-11-30 01:59:44 +0900

    :sparkles: Feat: 가게의 미션을 도전 중인 미션에 추가하기 API 구현
    멤버에 도전 미션 추가 기능 구현

--- src/main/java/umc/spring/web/controller/MemberRestController.java
+++ src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -2,16 +2,24 @@ package umc.spring.web.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import umc.spring.apiPayload.ApiResponse;
 import umc.spring.converter.MemberConverter;
+import umc.spring.converter.MemberMissionConverter;
+import umc.spring.converter.ReviewConverter;
 import umc.spring.domain.Member;
+import umc.spring.domain.Review;
+import umc.spring.domain.mapping.MemberMission;
+import umc.spring.service.MemberMissionService.MemberMissionCommandService;
 import umc.spring.service.MemberService.MemberCommandService;
+import umc.spring.validation.annotation.ChallengingMissions;
+import umc.spring.validation.annotation.ExistMembers;
+import umc.spring.validation.annotation.ExistMissions;
+import umc.spring.validation.annotation.ExistRestaurants;
 import umc.spring.web.dto.MemberRequestDTO;
 import umc.spring.web.dto.MemberResponseDTO;
+import umc.spring.web.dto.ReviewRequestDTO;
+import umc.spring.web.dto.ReviewResponseDTO;
 
 import javax.validation.Valid;
 
@@ -22,10 +30,18 @@ import javax.validation.Valid;
 public class MemberRestController {
 
     private final MemberCommandService memberCommandService;
+    private final MemberMissionCommandService memberMissionCommandService;
 
     @PostMapping("/join")
     public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDTO request){
         Member member = memberCommandService.joinMember(request);
         return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
+    }
+
+    @PostMapping("/me/missions/{missionId}")
+    public ApiResponse<MemberResponseDTO.ChallengeMissionDTO> challenge(@ExistMembers @RequestParam(name = "memberId") Long memberId,
+                                                                        @ExistMissions @ChallengingMissions @PathVariable(name = "missionId") Long missionId){
+        MemberMission challengeMission = memberMissionCommandService.challengeMission(missionId, memberId);
+        return ApiResponse.onSuccess(MemberMissionConverter.toChallengMissionDTO(challengeMission));
     }
 }
