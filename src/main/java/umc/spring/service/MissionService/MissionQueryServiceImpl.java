309aeb6dcffa4fb8c6293e498b5e4f966aaa2b28
GyuHyeokjjookki <jjookki33@gmail.com>
2023-12-04 17:28:28 +0900

    :sparkles: Feat: 특정 가게의 미션 목록 조회 API 구현
    가게의 미션 목록 조회를 위한 service logic 구현

--- src/main/java/umc/spring/service/MissionService/MissionQueryServiceImpl.java
+++ src/main/java/umc/spring/service/MissionService/MissionQueryServiceImpl.java
@@ -1,9 +1,14 @@
 package umc.spring.service.MissionService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.spring.domain.Mission;
+import umc.spring.domain.Restaurant;
 import umc.spring.repository.MissionRepository;
+import umc.spring.repository.RestaurantRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -11,10 +16,20 @@ import umc.spring.repository.MissionRepository;
 public class MissionQueryServiceImpl implements MissionQueryService{
 
     private final MissionRepository missionRepository;
+    private final RestaurantRepository restaurantRepository;
 
     @Override
     @Transactional
     public boolean existMissionById(Long id) {
         return missionRepository.existsById(id);
+    }
+
+    @Override
+    @Transactional
+    public Page<Mission> getMissionList(Long restaurantId, Integer page) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).get();
+
+        Page<Mission> missionPage = missionRepository.findAllByRestaurant(restaurant, PageRequest.of(page, 10));
+        return missionPage;
     }
 }
