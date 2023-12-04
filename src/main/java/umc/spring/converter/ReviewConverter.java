6639570d76dbc6b9d3306c714f7f0751872e958b
GyuHyeokjjookki <jjookki33@gmail.com>
2023-12-04 14:47:59 +0900

    :sparkles: Feat: 가게 리뷰 목록 조회 API 구현
    리뷰 조회 converter 구현

--- src/main/java/umc/spring/converter/ReviewConverter.java
+++ src/main/java/umc/spring/converter/ReviewConverter.java
@@ -1,10 +1,13 @@
 package umc.spring.converter;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Review;
 import umc.spring.web.dto.ReviewRequestDTO;
 import umc.spring.web.dto.ReviewResponseDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReviewConverter {
 
@@ -19,6 +22,30 @@ public class ReviewConverter {
         return Review.builder()
                 .content(request.getContent())
                 .rating(request.getRating())
+                .build();
+    }
+
+    public static ReviewResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review){
+        return ReviewResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickName(review.getMember().getName())
+                .rating(review.getRating())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .content(review.getContent())
+                .build();
+    }
+
+    public static ReviewResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList){
+
+        List<ReviewResponseDTO.ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(ReviewConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return ReviewResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst((reviewList.isFirst()))
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
                 .build();
     }
 }
