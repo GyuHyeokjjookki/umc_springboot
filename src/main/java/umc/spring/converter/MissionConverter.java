d6b8931d8d0b2ca26eb3f1dbec2b351c20577fb1
GyuHyeokjjookki <jjookki33@gmail.com>
2023-12-04 17:27:00 +0900

    :sparkles: Feat: 특정 가게의 미션 목록 조회 API 구현
    가게의 미션 목록 조회를 위한 converter 구현

--- src/main/java/umc/spring/converter/MissionConverter.java
+++ src/main/java/umc/spring/converter/MissionConverter.java
@@ -1,6 +1,8 @@
 package umc.spring.converter;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Mission;
+import umc.spring.domain.Restaurant;
 import umc.spring.domain.Review;
 import umc.spring.web.dto.Mission.MissionRequestDTO;
 import umc.spring.web.dto.Mission.MissionResponseDTO;
@@ -8,6 +10,9 @@ import umc.spring.web.dto.ReviewRequestDTO;
 import umc.spring.web.dto.ReviewResponseDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MissionConverter {
 
@@ -23,6 +28,31 @@ public class MissionConverter {
                 .reward(request.getReward())
                 .deadline(request.getDeadLine())
                 .content(request.getContent())
+                .build();
+    }
+
+    public static MissionResponseDTO.MissionPreViewDTO missionPreViewDTO(Mission mission){
+        return MissionResponseDTO.MissionPreViewDTO.builder()
+                .reward(mission.getReward())
+                .deadLine(mission.getDeadline())
+                .content(mission.getContent())
+                .createdAt(mission.getCreatedAt().toLocalDate())
+                .build();
+    }
+
+    public static MissionResponseDTO.MissionPreViewListDTO missionPreViewListDTO(Page<Mission> missionList, Optional<Restaurant> restaurant){
+
+        List<MissionResponseDTO.MissionPreViewDTO> missionPreViewDTOList = missionList.stream()
+                .map(MissionConverter::missionPreViewDTO).collect(Collectors.toList());
+
+        return MissionResponseDTO.MissionPreViewListDTO.builder()
+                .restaurantName(restaurant.get().getName())
+                .isLast(missionList.isLast())
+                .isFirst((missionList.isFirst()))
+                .totalPage(missionList.getTotalPages())
+                .totalElements(missionList.getTotalElements())
+                .listSize(missionPreViewDTOList.size())
+                .missionList(missionPreViewDTOList)
                 .build();
     }
 }
