524a8384081c20df8403e4cb9ffb66ea3f3f9113
GyuHyeokjjookki <jjookki33@gmail.com>
2023-12-04 14:49:31 +0900

    :sparkles: Feat: 가게 리뷰 목록 조회 API 구현
    식당 조회 service 코드 작성

--- src/main/java/umc/spring/service/RestaurantService/RestaurantQueryService.java
+++ src/main/java/umc/spring/service/RestaurantService/RestaurantQueryService.java
@@ -1,6 +1,12 @@
 package umc.spring.service.RestaurantService;
 
+import umc.spring.domain.Restaurant;
+
+import java.util.Optional;
+
 public interface RestaurantQueryService {
 
     public boolean existRestaurantById(Long id);
+
+    public Optional<Restaurant> findRestaurant(Long id);
 }
