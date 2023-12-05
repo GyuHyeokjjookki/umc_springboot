eb9dfa696580464738f2bb2f575b5f17f801b317
GyuHyeokjjookki <jjookki33@gmail.com>
2023-12-05 14:10:40 +0900

    :sparkles: Feat: 내가 진행중인 미션 목록 조회 API 구현
    member mission service code for my mission list

--- src/main/java/umc/spring/service/MemberMissionService/MemberMissionQueryService.java
+++ src/main/java/umc/spring/service/MemberMissionService/MemberMissionQueryService.java
@@ -1,9 +1,13 @@
 package umc.spring.service.MemberMissionService;
 
+import org.springframework.data.domain.Page;
+import umc.spring.domain.Mission;
 import umc.spring.domain.mapping.MemberMission;
 
 import java.util.Optional;
 
 public interface MemberMissionQueryService {
     MemberMission findMemberMission(Long missionId);
+
+    public Page<MemberMission> getMyMissionList(Long memberId, Integer page);
 }
