c7216a1e75bf06e0798bfa7471e7f89cc399147f
GyuHyeokjjookki <jjookki33@gmail.com>
2023-12-05 16:32:51 +0900

    :sparkles: Feat: 진행중인 미션 진행 완료로 바꾸기 API 구현
    미션 완료를 위한 service logic 구현

--- src/main/java/umc/spring/service/MemberMissionService/MemberMissionCommandService.java
+++ src/main/java/umc/spring/service/MemberMissionService/MemberMissionCommandService.java
@@ -5,4 +5,6 @@ import umc.spring.domain.mapping.MemberMission;
 public interface MemberMissionCommandService {
 
     public MemberMission challengeMission(Long missionId, Long memberId);
+
+    public MemberMission completeMission(Long missionId, Long memberId);
 }
